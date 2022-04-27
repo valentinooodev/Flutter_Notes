b06ba50a3d35a83a8466c3a1029509bb80a60f64
valentinooodev <valentinooo.dev@gmail.com>
2022-04-27 14:36:24 +0700

    Separating App Initialization from Login and Register Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:notes/views/register_view.dart';
 import 'package:notes/views/login_view.dart';
+
 import 'firebase_options.dart';
 
 void main() {
@@ -13,11 +13,39 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     ),
   );
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('Email verified');
+              } else {
+                print('You need to verify your email');
+              }
+              return const Text('Done');
+            default:
+              return const Text('Loading');
+          }
+        },
+      ),
+    );
+  }
+}
